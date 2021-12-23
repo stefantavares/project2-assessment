5c7f8c457f53e859bbe05166bc7832fae516401d
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-12-23 12:56:01 -0800

    Finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const todos = require('../data/todos');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Unit 2 Asessment', todos });
 });
+
+// POST '/todos' - Create Route
+router.post('/todos', function (req, res) {
+  req.body.done = false;
+  todos.push(req.body);
+  console.log(todos);
+  res.redirect('/');
+});
+
+// DELETE "/todos/:idx" - Delete Route
+router.delete('/todos/:idx', function (req, res) {
+  todos.splice(req.params.idx, 1);
+  res.redirect('/');
+})
+
 
 module.exports = router;
